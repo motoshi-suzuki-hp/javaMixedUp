0dcb86d9a0023c49d9848f1bf53ae9efbb7d0ee9
Motoshi0711 <motoshi.suzuki.hp@gmail.com>
2022-08-17 16:15:16 +0900

    java

--- src/java7.java
+++ src/java7.java
@@ -1,5 +1,96 @@
+import java.io.*;
+
 public class java7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // // 7-1
+        // Dog dog1 = new Dog();
+        // dog1.SetName("Jhon");
+        // dog1.ShowProfile();
+
+        // // 7-2
+        // Dog dog2 = new Dog();
+        // dog2.SetName("Tom");
+        // dog2.SetAge(5);
+        // dog2.ShowProfile();
+
+        // // 7-3
+        // Dog dog31 = new Dog();
+        // Dog dog32 = new Dog();
+
+        // dog31.SetName("Tom");
+        // dog32.SetName("Bob");
+
+        // dog31.SetAge(5);
+        // dog32.SetAge(3);
+
+        // dog31.ShowProfile();
+        // dog32.ShowProfile();
+
+        // // 7-4
+        // Dog dog4 = new Dog();
+
+        // dog4.SetBreed("Chihuahua");
+        // dog4.SetName("Tom");
+        // dog4.SetAge(5);
+        // dog4.ShowProfile();
+
+        // // 7-5
+        // Dog dog5 = new Dog("Chihuahua");
+
+        // dog5.SetName("Tom");
+        // dog5.SetAge(5);
+        // dog5.ShowProfile();
+
+        // // 7-6
+        // BufferedReader br6 = new BufferedReader(
+        //     new InputStreamReader( System.in ) );
+        // CoinCase coincase = new CoinCase();
+
+        // for(int i = 0; i < 10; i++) {
+        //     System.out.print( "硬貨の種類は?" );
+        //     int kind = Integer.parseInt( br6.readLine() );
+        //     System.out.print( "硬貨の枚数は?" );
+        //     int count = Integer.parseInt( br6.readLine() );
+
+        //     coincase.AddCoins(kind, count);
+        // }
+
+        // System.out.println("500円は" + coincase.GetCount(500) + "枚");
+        // System.out.println("100円は" + coincase.GetCount(100) + "枚");
+        // System.out.println("50円は" + coincase.GetCount(50) + "枚");
+        // System.out.println("10円は" + coincase.GetCount(10) + "枚");
+        // System.out.println("5円は" + coincase.GetCount(5) + "枚");
+        // System.out.println("1円は" + coincase.GetCount(1) + "枚");
+
+        // System.out.println("総額は" + coincase.GetAmount() + "円");
+
+        // 7-7
+        BufferedReader br7 = new BufferedReader(
+            new InputStreamReader( System.in ) );
+        CoinCase coincase = new CoinCase();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.print( "硬貨の種類は?" );
+            int kind = Integer.parseInt( br7.readLine() );
+            System.out.print( "硬貨の枚数は?" );
+            int count = Integer.parseInt( br7.readLine() );
+
+            coincase.AddCoins(kind, count);
+        }
+
+        System.out.println("500円は" + coincase.GetCount(500) + "枚");
+        System.out.println("100円は" + coincase.GetCount(100) + "枚");
+        System.out.println("50円は" + coincase.GetCount(50) + "枚");
+        System.out.println("10円は" + coincase.GetCount(10) + "枚");
+        System.out.println("5円は" + coincase.GetCount(5) + "枚");
+        System.out.println("1円は" + coincase.GetCount(1) + "枚");
+        System.out.println("全部で" + coincase.GetCount() + "枚");
+
+        System.out.println("総額は" + coincase.GetAmount() + "円");
+
 
     }
+
+
+
 }
---
+++ src/java8.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class java8 {
+    public static void main(String[] args) throws IOException {
+        // 8-1
+        
+    }
+}
