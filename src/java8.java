a59223559f862fe5d2d88f8144e4a61534feb422
Motoshi0711 <motoshi.suzuki.hp@gmail.com>
2022-08-18 12:23:29 +0900

    java

--- src/java8.java
+++ src/java8.java
@@ -3,6 +3,27 @@ import java.io.*;
 public class java8 {
     public static void main(String[] args) throws IOException {
         // 8-1
-        
+        Cat cat1 = new Cat();
+        cat1.Name = "Jhon";
+        cat1.Age = 2;
+        cat1.ShowProfile();
+        cat1.Sleep();
+
+    }
+}
+
+class Animal {
+    public String Name;
+    public int Age;
+
+    public void ShowProfile()
+    {
+        System.out.println( "名前は、" + Name + "、" + Age + "歳です。" );
+    }
+}
+
+class Cat extends Animal {
+    public void Sleep() {
+        System.out.println("スースー");
     }
 }
